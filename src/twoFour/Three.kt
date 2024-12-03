f572770eea54e3e01a8feca5972a5a7c245f5114
Leiber <mixer.wroc@gmail.com>
2024-12-03 20:35:51 +0100

    Advent of code 2024 Day3

---
+++ src/twoFour/Three.kt
@@ -0,0 +1,51 @@
+package twoFour
+
+import java.io.File
+import java.util.TreeMap
+import kotlin.concurrent.fixedRateTimer
+import kotlin.math.absoluteValue
+
+object Three {
+    fun resolve() {
+//        resolve1()
+        resolve2()
+    }
+
+    private fun resolve1() {
+        var mul = 0
+        File("24/input_three.txt").forEachLine { line ->
+            Regex("mul\\((\\d*),(\\d*)\\)").findAll(line).forEach {
+                val (a, b) = it.destructured
+                val m = a.toInt()*b.toInt()
+                println("$a $b")
+                println(m)
+                mul += m
+            }
+        }
+        println("mul is $mul")
+    }
+
+    private fun resolve2() {
+        var mul = 0
+        var count = true
+        File("24/input_three.txt").forEachLine { line ->
+            Regex("mul\\((\\d*),(\\d*)\\)|(do\\(\\))|(don\\'t\\(\\))").findAll(line).forEach {
+                val (a, b, doo, dont) = it.destructured
+                val aa = if (a.isNotEmpty()) a.toInt() else 0
+                val bb = if (b.isNotEmpty()) b.toInt() else 0
+                val dooo = doo.isNotEmpty()
+                val dontt = dont.isNotEmpty()
+                println("$a $b $doo $dont")
+                if(dooo) {
+                    count = true
+                } else if (dontt) {
+                    count = false
+                }
+                if(count) {
+                    mul += aa * bb
+                }
+            }
+        }
+        println("mul is $mul")
+    }
+}
